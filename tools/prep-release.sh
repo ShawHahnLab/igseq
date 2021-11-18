ab3a10d3b0d306b9621e1dc622ccbd9de884df3d
Jesse Connell <ancon@upenn.edu>
2021-11-18 12:35:14 -0500

    add prep-release tool

---
+++ tools/prep-release.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+version=$(python -c 'from igseq.version import __version__; print(__version__)')
+date=$(date +%Y-%m-%d)
+echo Version: $version
+sed -i 's/{% set version = ".*" %}/{% set version = "'$version'" %}'/ conda/meta.yaml
+sed -i "s/# dev$/# $version - $date/" CHANGELOG.md
+echo conda/meta.yaml vs igseq/data/environment.yml:
+diff -W60 -y \
+	<(grep '^  run:$' conda/meta.yaml -A 1000 | tail -n +2 | grep ^$ -B 1000 | head -n -1 | sed 's/^ *//') \
+	<(grep dependencies igseq/data/environment.yml -A 1000 | tail -n +2 | sed 's/^ *//')
+[ $? -eq 0 ] && echo ...OK! || echo "...fix that!"
