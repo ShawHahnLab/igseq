eebb5657b4409f7fa6672d661147d86416591002
Jesse Connell <ancon@upenn.edu>
2021-12-22 14:52:41 -0500

    replace tab2seq command with convert

--- igseq/convert.py
+++ igseq/convert.py
@@ -1,8 +1,24 @@
+"""
+Convert between various sequence and tabular file formats.
+
+Input and output formats are by default inferred from filenames but can be
+given explicitly if needed.  The formats are:
+
+    fa:    FASTA
+    fagz:  gzipped FASTA
+    fq:    FASTQ
+    fqgz:  gzipped FASTQ
+    csv:   comma-separated values
+    csvgz: gzipped comma-separated values
+    tsv:   tab-separated values
+    tsvgz: gzipped tab-separated values
+"""
+
 import logging
 from .record import RecordReader, RecordWriter
 
-def convert(input_fp, output_fp, input_fmt=None, output_fmt=None, dummyqual=None):
-    with RecordReader(input_fp, input_fmt) as reader, \
-        RecordWriter(output_fp, output_fmt, dummyqual=dummyqual) as writer:
+def convert(path_in, path_out, fmt_in=None, fmt_out=None, dummyqual=None):
+    with RecordReader(path_in, fmt_in) as reader, \
+        RecordWriter(path_out, fmt_out, dummyqual=dummyqual) as writer:
         for record in reader:
             writer.write(record)
