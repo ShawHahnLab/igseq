b7b0ba90ca4c68ab91a5a60045bb4ae9a29298b5
Jesse Connell <ancon@upenn.edu>
2023-04-05 17:15:56 -0400

    update channel order for conda build script

--- tools/condabuild.sh
+++ tools/condabuild.sh
@@ -2,6 +2,6 @@
 
 # As helpfully explained in:
 # https://medium.com/analytics-vidhya/publish-a-python-package-to-conda-b352eb0bcb2e
-conda build -c bioconda -c conda-forge -c defaults conda --python=3.9 --output-folder conda-out
+conda build -c conda-forge -c bioconda -c defaults conda --python=3.9 --output-folder conda-out
 # Then just:
 # anaconda upload -u ShawHahnLab conda-out/linux-64/igseq-0.0.1-py39_0.tar.bz2
