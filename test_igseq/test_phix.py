a7a183a598ad9756e0abd6dde4e189baacd7c700
Jesse Connell <ancon@upenn.edu>
2024-05-17 12:15:13 -0400

    For #68: failing test of phix with counts CSV path

--- test_igseq/test_phix.py
+++ test_igseq/test_phix.py
@@ -25,3 +25,17 @@ class TestPhixLive(TestBase, TestLive):
             # does too.
             with open(path/"phix/run/phix.counts.csv", encoding="ASCII") as f_in:
                 self.assertEqual(f_in.read(), "Category,Sample,Item,NumSeqs\nphix,,mapped,1\n")
+
+    def test_phix_custom_counts(self):
+        with TemporaryDirectory() as temp:
+            path = Path(temp)
+            copytree(self.path/"input", path/"input")
+            phix([path/"input/run"], counts_out=str(path/"phix/run/custom_counts_path.csv"))
+            # Check that the output filenames all match up
+            files = sorted([p.name for p in (path/"phix/run").glob("*")])
+            files_expected = sorted(["phix.bam", "custom_counts_path.csv"])
+            self.assertEqual(files_expected, files)
+            # Check the read counts.  If this looks right we'll assume the bam
+            # does too.
+            with open(path/"phix/run/custom_counts_path.csv", encoding="ASCII") as f_in:
+                self.assertEqual(f_in.read(), "Category,Sample,Item,NumSeqs\nphix,,mapped,1\n")
